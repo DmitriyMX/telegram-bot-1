d69dfeb52497d0a684dfde5bff82ede07a8c223a
DmitriyMX <dimon550@gmail.com>
2018-11-28 18:16:17 +0300

    добавил комманды

--- src/main/java/ru/dmitriymx/tlgbot/TlgBot.java
+++ src/main/java/ru/dmitriymx/tlgbot/TlgBot.java
@@ -1,15 +1,17 @@
 package ru.dmitriymx.tlgbot;
 
 import lombok.extern.slf4j.Slf4j;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.dmitriymx.tlgbot.commands.HelpCommand;
+import ru.dmitriymx.tlgbot.commands.StartCommand;
 import ru.dmitriymx.tlgbot.service.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-public class TlgBot extends TelegramLongPollingBot {
+public class TlgBot extends TelegramLongPollingCommandBot {
     private static final String TOKEN = Utils.getVariable("TLG_TOKEN");
     private static final String NAME  = Utils.getVariable("TLG_NAME");
     private List<Service> services = new ArrayList<>();
@@ -19,22 +21,24 @@ public class TlgBot extends TelegramLongPollingBot {
         log.debug("Bot token: {}", TOKEN);
     }
 
+    public TlgBot() {
+        super(NAME);
+
+        register(new StartCommand());
+        register(new HelpCommand());
+    }
+
     public void addService(Service service) {
         services.add(service);
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-        log.trace("onUpdateReceived.enter; update={}", update);
+    public void processNonCommandUpdate(Update update) {
+        log.trace("processNonCommandUpdate.enter; update={}", update);
 
         services.forEach(service -> service.handle(new TelegramData(this, update)));
 
-        log.trace("onUpdateReceived.exit; update={}", update);
-    }
-
-    @Override
-    public String getBotUsername() {
-        return NAME;
+        log.trace("processNonCommandUpdate.exit; update={}", update);
     }
 
     @Override
