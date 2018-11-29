c4f9206edbda426fffd4a0d6da5bdc60140912a1
DmitriyMX <dimon550@gmail.com>
2018-11-29 14:23:44 +0300

    добавлена работа через прокси

--- src/main/java/ru/dmitriymx/tlgbot/Main.java
+++ src/main/java/ru/dmitriymx/tlgbot/Main.java
@@ -1,7 +1,13 @@
 package ru.dmitriymx.tlgbot;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import ru.dmitriymx.tlgbot.service.EchoService;
@@ -13,7 +19,24 @@ public class Main {
         ApiContextInitializer.init();
         TelegramBotsApi api = new TelegramBotsApi();
 
-        TlgBot bot = new TlgBot();
+        TlgBot bot;
+
+        if (Utils.getVariable("PROXY_HOST") != null) {
+            final String PROXY_HOST = Utils.getVariable("PROXY_HOST");
+            final int PROXY_PORT = Integer.parseInt(Utils.getVariable("PROXY_PORT"));
+
+            log.debug("using proxy: {}:{}", PROXY_HOST, PROXY_PORT);
+
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+            botOptions.setProxyHost(PROXY_HOST);
+            botOptions.setProxyPort(PROXY_PORT);
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
+
+            bot = new TlgBot(botOptions);
+        } else {
+            bot = new TlgBot();
+        }
+
         bot.addService(new EchoService("Echo: "));
 
         try {
